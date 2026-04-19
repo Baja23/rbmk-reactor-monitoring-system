3fd8b639932be5144c47c279fe750f82701d8447
Barbara Składanowska <bskladanowska@gmail.com>
2026-04-19 12:26:48 +0200

    Corrected class default values and added a loop ina pp.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from schemas import Reactor as r
+from schemas import Reactor, reactor_run
 
 def main():
+    reactor = Reactor()
+    counter = 0
     while True:
+        reactor_run(reactor, 1.0)
+        print(reactor.thermal_power_mw, reactor.reactivity_delta, reactor.xenon_level)
     #pętla programu 
     #połączenie z bazą danych
     #pobranie danych z bazy danych
@@ -11,4 +15,5 @@ def main():
     #Alarmujące wartości 
     #user input po wciśnięciu jakiegoś przycisku w terminalu
     #po wpisaniu AZ5 wsunięcie wszystkich prętów kontrolnych
-    pass
+if __name__ == "__main__":
+    main()
