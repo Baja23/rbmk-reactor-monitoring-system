dfcae039ed378ce10533642db569dd6fc0df3966
thisismyusername420 <tycjan12@gmail.com>
2026-06-14 23:10:56 +0200

    Consolidated 4 api points into single one, removed CORS, added API key verification, removed automatic public documentation of API for safety reasons, added Redis error check, modernized removing data stored in Redis on startup

--- api.py
+++ api.py
@@ -1,75 +1,59 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
 from redis_client import r
+from redis.exceptions import ConnectionError as RedisConnectionError
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-app = FastAPI()
-
-# CORS(Cross-Origin Resource Sharing)
-app.add_middleware( # By default, internet browsers and servers block the requests if scripts tries to send data to API that works under different address
-    CORSMiddleware, # CORS allows these requests to reach the API
-    allow_origins=["*"], # This means allows request from any URL address
-    allow_methods=["*"], # This means approval to use any HTTP method (GET, POST etc.)
-    allow_headers=["*"], # This means approval to send any headers (Content-Type: application/json)
-)
-
-# BaseModel enabled automatic parsing, validation and generating documentation
-class GrafanaData(BaseModel):
-    value: int | None = None
-
-
-
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     '''Delete leftover data on startup'''
 
     r.delete("rods_movement")
     r.delete("water_flow")
     r.delete("in_water_temp")
     r.delete("az5")
+    yield
 
-@app.post("/prety")
-async def grafana_webhook_rods(data: GrafanaData):
-    '''Take data about rod movement from Grafana'''
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
+)
 
-    print(f"Otrzymano wartość przez POST pręty: {data.value}", flush=True)
-    if data.value is None:
-        return("Value is None.")
-    r.lpush("rods_movement", data.value)
-    # return int(value)
+API_KEY = os.getenv("X_API_KEY")
+domain_url = os.getenv("DOMAIN_URL")
 
-
-@app.post("/water")
-async def grafana_webhook_water(data: GrafanaData):
-    '''Take data about water flow from Grafana'''
-
-    print(f"Otrzymano wartość przez POST water: {data.value}", flush=True)
-    if data.value is None:
-        return("Value is None.")
-    r.lpush("water_flow", data.value)
+# BaseModel enabled automatic parsing, validation and generating documentation
+class GrafanaData(BaseModel):
+    value: int | None = None
+    name: str | None = None
+    key: str | None = None
 
 
-@app.post("/in_water_temp")
-async def grafana_webhook_water(data: GrafanaData):
-    '''Take data about inlet water temperature from Grafana'''
+def verify_button_header(data:GrafanaData):
+    '''Verify API key send in payload from Grafana'''
+    if data.key != API_KEY:
+        raise HTTPException(status_code=403, detail="Unauthorized access")
 
-    print(f"Otrzymano wartość przez POST in_water_temp: {data.value}", flush=True)
-    if data.value is None:
-        return("Value is None.")
-    r.lpush("in_water_temp", data.value)
+# Launches a function that verifies API key
+@app.post("/webhook", dependencies=[Depends(verify_button_header)])
+async def grafana_webhook(data: GrafanaData):
+    '''Take data of attributes from Grafana'''
 
+    print(f"Otrzymano wartości z Grafana przez POST - {data.name}: {data.value}")
+    try:
+        if data.value is None:
+            return("Value is None.")
+        r.lpush(f"{data.name}", data.value)
+    except RedisConnectionError:
+        raise HTTPException(status_code=503, detail="Redis database is unavailable!")
 
-@app.post("/az5")
-async def grafana_webhook_az5(data: GrafanaData):
-    '''Take information about AZ5 procedure initialization'''
-
-    print(f"Otrzymano sygnał AZ5 {data.value}")
-    if data.value is None:
-        return ("Value is None.")
-    r.lpush("az5", data.value)
 
 
 
